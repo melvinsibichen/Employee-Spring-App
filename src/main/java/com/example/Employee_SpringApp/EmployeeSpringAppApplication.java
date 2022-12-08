8346cbf2895ed2dda0d2fa4f1beaaa2377c489bc
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-08 20:45:10 +0530

    first commit

---
+++ src/main/java/com/example/Employee_SpringApp/EmployeeSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee_SpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Employee_SpringApp/EmployeeSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Employee_SpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
